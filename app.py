9b6ad602142b811236e819bf5663de04502aca82
Pragati Lohia <lohiapragati25112007@gmail.com>
2026-02-07 18:04:23 +0530

    Refactor model loading in app.py
    Simplified model loading by removing dynamic path construction.

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 
-import os
-model_path = os.path.join(os.path.dirname(__file__), "model.pkl")
-model = pickle.load(open(model_path, "rb"))
+model = pickle.load(open("model.pkl", "rb"))
 
 st.title("Insurance Cost Predictor")
 
